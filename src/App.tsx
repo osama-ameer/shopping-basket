1ae7d0ba928289c5a8c8dc554fbd42435260277c
Osama Ameer <osamaameer15@gmail.com>
2021-02-22 01:44:36 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Basket } from './components/Basket';
+import { Product } from './components/Product';
+import { Container, Box } from "@material-ui/core"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <Container maxWidth="md">
+        <Box mt={5} mb={5}>
+          <Product />
+        </Box>
+        <Box mt={5} mb={5}>
+          <Basket />
+        </Box>
+      </Container>
     </div>
   );
 }
---
+++ src/store/index.ts
@@ -0,0 +1,42 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { INITIAL_STATE } from "./state";
+
+
+//reducer
+const basketSlice = createSlice({
+    name: "basket",
+    initialState: INITIAL_STATE,
+    reducers: {
+        add: (state, action) => {
+
+           return state.map( item => {
+               if(item.id !== action.payload.id){
+                   return item
+               }
+               return {
+                   ...item,
+                   added: true
+
+               }
+           })
+
+        },
+        remove: (state, action) => {
+            return state.map( item => {
+                if( item.id !== action.payload.id){
+                    return item
+                }
+                return {
+                    ...item,
+                    added:false
+                }
+            })
+        }
+    }
+})
+
+const store = configureStore({ reducer: basketSlice.reducer });
+
+export const { add , remove } = basketSlice.actions
+
+export { basketSlice, store }
